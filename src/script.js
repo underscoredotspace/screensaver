39903844eff06cb7bbaa7e9d2c02f84700658ecf
Colin Tindle <ampersand@underscore.space>
2018-01-02 21:44:13 +0000

    Improved trails

--- src/script.js
+++ src/script.js
@@ -24,6 +24,7 @@ class Shape {
     this.pos = []
     this.speed = []
     this.trails = []
+    this.colour = 0
 
     this.context = context
     for (let v = 0; v < points; v++) {
@@ -51,8 +52,13 @@ class Shape {
 
   draw() {
     const context = this.context
+    const [r,g,b] = hslToRgb(this.colour, 1, 0.5)
+    context.strokeStyle = `rgb(${r}, ${g}, ${b})`
     this._draw()
-    for (let trail of this.trails) {
+    for (let ndx in this.trails) {
+      const opacity = ((1 / this.trails.length) * ndx)
+      const trail = this.trails[ndx]
+      context.strokeStyle = `rgba(${r}, ${g}, ${b}, ${opacity})`
       this._draw(trail)
     }
   }
@@ -72,12 +78,18 @@ class Shape {
 
     let trail = []
     for (let pos of this.pos) {
-      trail.push({...pos})
+      trail.push({x:pos.x,y:pos.y})
     }
     this.trails.push(trail)
     if(this.trails.length >= 200) {
       this.trails = this.trails.slice(this.trails.length-199, 200)
     }
+    
+    this.colour += 1/360/10
+    if (this.colour >= 1) {
+      this.colour = 0
+    }
+
   }
 }
 
@@ -92,7 +104,7 @@ function start() {
   // called when window is resized
   window.addEventListener('resize', () => resizeCanvas(canvas))
 
-  const poly = new Shape(context, 4)
+  const poly = new Shape(context, 3)
   draw(poly, context)
 }
 
@@ -116,3 +128,39 @@ function draw(poly, context) {
 }
 
 start()
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   {number}  h       The hue
+ * @param   {number}  s       The saturation
+ * @param   {number}  l       The lightness
+ * @return  {Array}           The RGB representation [r, g, b]
+ */
+function hslToRgb(h, s, l){
+  var r, g, b;
+
+  if(s == 0){
+      r = g = b = l // achromatic
+  }else{
+      function hue2rgb(p, q, t){
+          if(t < 0) t += 1
+          if(t > 1) t -= 1
+          if(t < 1/6) return p + (q - p) * 6 * t
+          if(t < 1/2) return q
+          if(t < 2/3) return p + (q - p) * (2/3 - t) * 6
+          return p
+      }
+
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s
+      const p = 2 * l - q
+      r = hue2rgb(p, q, h + 1/3)
+      g = hue2rgb(p, q, h)
+      b = hue2rgb(p, q, h - 1/3)
+  }
+
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)]
+}
